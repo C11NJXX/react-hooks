716aec79a9d72915bbdf1f4ae67c0ee3eef11528
C_11nJxx <c_11njxxsmailbox@sina.com>
2025-03-25 11:52:23 +0800

    Init

---
+++ react-hooks/src/App.tsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <>
+      <div className="h-screen bg-pink-200 flex justify-center items-center">
+        <h1 className="text-pink-700 text-4xl font-bold">Show All React Hooks</h1>
+      </div>
+    </>
+  )
+}
